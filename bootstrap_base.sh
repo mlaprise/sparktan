0af34e5875645f71a499e50ed0e48ccd3fb566ca
Martin Laprise <mlaprise@gmail.com>
2015-10-14 14:49:04 -0400

    Add bootstrap bash script for EMR

---
+++ bootstrap_base.sh
@@ -0,0 +1,13 @@
+#
+# Install some basic dev tools we might need.
+#
+#@sudo yum update -y
+sudo yum install make automake gcc gcc-c++ kernel-devel git-core -y
+sudo yum install htop -y
+
+#
+# Bootstrap venvs
+#
+sudo pip-2.7 install virtualenv
+sudo pip-2.7 install virtualenvwrapper
+mkdir /home/hadoop/virtualenvs
